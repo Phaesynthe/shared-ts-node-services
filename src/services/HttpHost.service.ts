304ab173b0ca7c0a61c1b4cb92de67555c8d4404
Matthew W. Jackson <mwj8410@gmail.com>
2023-02-02 15:28:57 -0500

    feat: merge route mapping into the shared module

--- src/services/HttpHost.service.ts
+++ src/services/HttpHost.service.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import {HttpMethod} from "../models";
 
 export class HttpHostService {
     private app: express.Application;
@@ -36,6 +37,34 @@ export class HttpHostService {
     public stop() {
         if (this.server) {
             this.server.close();
+        }
+    }
+
+    public mountRoutes(router, routeDefs): void {
+        let methodFormatted: string;
+        for (const routeDef of routeDefs) {
+            switch (routeDef.method) {
+                case HttpMethod.Delete:
+                    router.delete(routeDef.uri, routeDef.handler);
+                    methodFormatted = routeDef.method.padEnd(10);
+                    break;
+                case HttpMethod.Get:
+                    router.get(routeDef.uri, routeDef.handler);
+                    methodFormatted = routeDef.method.padEnd(10);
+                    break;
+                case HttpMethod.Patch:
+                    router.patch(routeDef.uri, routeDef.handler);
+                    methodFormatted = routeDef.method.padEnd(10);
+                    break;
+                case HttpMethod.Post:
+                    router.post(routeDef.uri, routeDef.handler);
+                    methodFormatted = routeDef.method.padEnd(10);
+                    break;
+                default:
+                    console.error(`Unmapped Method: ${routeDef.method}`)
+                    break;
+            }
+            console.log(`Mounted: ${methodFormatted} ${routeDef.uri}`);
         }
     }
 }
